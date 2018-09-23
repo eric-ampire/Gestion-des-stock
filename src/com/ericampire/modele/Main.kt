77a2432808d27da52cc0d3be5babce370f1969ac
Eric Ampire <ericampire.top@gmail.com>
2018-09-23 15:59:13 +0200

    Recuperation du choix de l'utilisateur

--- src/com/ericampire/modele/Main.kt
+++ src/com/ericampire/modele/Main.kt
@@ -1,7 +1,15 @@
 package com.ericampire.modele
 
+import java.io.Console
+
+var choixMenu: Int? = null
+
 fun main(args: Array<String>) {
     afficherMenu()
+}
+
+fun clearTerminal() {
+    Runtime.getRuntime().exec("clear")
 }
 
 fun afficherMenu() {
@@ -16,4 +24,13 @@ fun afficherMenu() {
     """.trimIndent()
 
     print(menu)
+
+    try {
+        choixMenu = readLine()!!.toInt()
+    } catch (e: Exception) {
+
+        clearTerminal()
+        print("VALEUR INCORRECT !!! \n\n")
+        print(menu)
+    }
 }
