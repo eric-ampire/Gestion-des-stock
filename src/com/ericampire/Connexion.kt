5bd453b160f26ab77f329ff94260a1103ac7a7e9
Eric Ampire <ericampire.top@gmail.com>
2018-09-23 16:11:48 +0200

    Mise en place du module permettant la connexion avec la base des données

--- src/com/ericampire/Connexion.kt
+++ src/com/ericampire/Connexion.kt
@@ -1,2 +1,27 @@
 package com.ericampire
 
+import java.sql.DriverManager
+import java.sql.Connection
+import java.sql.SQLException
+
+object Connexion {
+    private const val url = "jdbc:mysql://localhost/stock"
+    private const val username = "root"
+    private const val password = "root"
+
+    private var connexion: Connection? = null
+
+
+    fun getConnexion(): Connection? {
+        try {
+            Class.forName("com.mysql.jdbc.Driver")
+            connexion = DriverManager.getConnection(url, username, password)
+        } catch (e: ClassNotFoundException) {
+            println(e.message)
+        } catch (e: SQLException) {
+            println(e.message)
+        } finally {
+            return connexion
+        }
+    }
+}
