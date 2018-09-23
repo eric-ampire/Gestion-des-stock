fc1a8ad13054b2bf2ceb717cf91fdb413e37c915
Eric Ampire <ericampire.top@gmail.com>
2018-09-23 15:47:14 +0200

    Mise en place de la fonction servant a afficher les menu

--- src/com/ericampire/modele/Main.kt
+++ src/com/ericampire/modele/Main.kt
@@ -1,2 +1,19 @@
 package com.ericampire.modele
 
+fun main(args: Array<String>) {
+    afficherMenu()
+}
+
+fun afficherMenu() {
+    val menu = """
+        1 - Rechercher un article
+        2 - Ajouter un article
+        3 - Supprimer un article
+        4 - Modifier un article
+        5 - Afficher tout les articles
+        6 - Quit
+        >
+    """.trimIndent()
+
+    print(menu)
+}
