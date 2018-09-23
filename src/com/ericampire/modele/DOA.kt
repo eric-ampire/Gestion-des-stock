30f5940536dc223b83c9569df2a5d481afcc0ef6
Eric Ampire <ericampire.top@gmail.com>
2018-09-23 16:44:23 +0200

    Je viens de deplacer le contenu du fichier Model en rapport avec le doa Vers le fichier DOA.kt

--- src/com/ericampire/modele/DOA.kt
+++ src/com/ericampire/modele/DOA.kt
@@ -1,2 +1,81 @@
 package com.ericampire.modele
 
+import com.ericampire.Connexion
+
+class ArticleDOA : DOA<Article> {
+    private val connexion = Connexion.getConnexion()
+
+    override fun insert(item: Article) {
+        val preparedStatement = connexion?.prepareStatement("INSERT INTO article values(?, ?, ?, ?)")
+
+        with(preparedStatement!!) {
+            setInt(1, item.numero)
+            setDouble(2, item.prix)
+            setInt(3, item.quantity)
+            setString(4, item.nom)
+        }
+
+        preparedStatement.executeUpdate()
+    }
+
+    override fun delete(id: Int) {
+        val preparedStatement = connexion?.prepareStatement("DELETE FROM article WHERE numero = ?")
+
+        with(preparedStatement!!) {
+            setInt(1, id)
+        }
+
+        preparedStatement.execute()
+    }
+
+    override fun update(item: Article) {
+
+    }
+
+    override fun getItem(id: Int): Article? {
+        val preparedStatement = connexion?.prepareStatement("SELECT * FROM article WHERE numero = ?")
+        preparedStatement?.setInt(1, id)
+
+        val resultSet = preparedStatement?.executeQuery()
+
+        if (resultSet!!.next()) {
+
+            return Article(
+                    resultSet.getInt("numero"),
+                    resultSet.getString("nom"),
+                    resultSet.getDouble("prix"),
+                    resultSet.getInt("quantity")
+            )
+        }
+
+        return null
+    }
+
+    override fun getAllItem(): List<Article> {
+        val preparedStatement = connexion?.prepareStatement("SELECT * FROM article")
+        val resultSet = preparedStatement?.executeQuery()
+
+        val articles = arrayListOf<Article>()
+
+        while (resultSet!!.next()) {
+            val article = Article(
+                    resultSet.getInt("numero"),
+                    resultSet.getString("nom"),
+                    resultSet.getDouble("prix"),
+                    resultSet.getInt("quantity")
+            )
+
+            articles += article
+        }
+
+        return articles.toList()
+    }
+}
+
+interface DOA<T> {
+    fun insert(item: T)
+    fun delete(id: Int)
+    fun update(item: T)
+    fun getItem(id: Int): Article?
+    fun getAllItem(): List<T>
+}
